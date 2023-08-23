09d96d65eecc4fb89cbcfd225186384a2957a0f3
MatsRahmat <matsrahmathidayat@gmail.com>
2023-08-23 22:52:57 +0700

    build(redux): initialize redux and react router

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,23 @@
+import Button from "../components/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { tes } from "../stores/actions/actionCreator";
+
+export default function HomePage() {
+  const dispatc = useDispatch();
+  const { loading } = useSelector((state) => state.user);
+
+  const tesButton = (e) => {
+    e.preventDefault();
+    dispatc(tes(!loading));
+  };
+
+  return (
+    <>
+      <h1 className="text-2xl">Hello world</h1>
+      <Button callback={tesButton} />
+      <div>
+        Loading is <span>{loading ? 'true': 'false'}</span>
+      </div>
+    </>
+  );
+}
