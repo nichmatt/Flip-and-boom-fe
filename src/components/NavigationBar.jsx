3f5a858e3247c766580be28da72e9baae54bb614
nichmatt <nicholas.official17@gmail.com>
2023-08-25 13:09:44 +0700

    test navbar

---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,10 @@
+export default function NavigationBar() {
+  return (
+    <>
+      <section id="NavigationBar-Section">
+        <div id="Top-Navigation"></div>
+        <div id="Botom-Navigation"></div>
+      </section>
+    </>
+  );
+}
