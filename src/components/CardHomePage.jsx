2b4ae0d3e3c7c4d9676c86a3a74bc55e81e8551f
MatsRahmat <matsrahmathidayat@gmail.com>
2023-08-25 14:46:07 +0700

    feat: styling home page

---
+++ src/components/CardHomePage.jsx
@@ -0,0 +1,9 @@
+export default function CardHomePage({ urlCard }){
+    return (
+        <>
+            <div>
+                <img src={urlCard} alt="card" className="w-[20rem]" />
+            </div>
+        </>
+    )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,21 +2,38 @@ import Button from "../components/Button";
 import SIdeBar from "../components/SideBar";
 import { useDispatch, useSelector } from "react-redux";
 import { tes } from "../actionCreators";
+import CardHomePage from "../components/CardHomePage";
 
 export default function HomePage() {
-	const dispatch = useDispatch();
-	const { loading } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const { loading } = useSelector((state) => state.user);
 
-	const tesButton = (e) => {
-		e.preventDefault();
-		dispatch(tes(!loading));
-	};
+  const tesButton = (e) => {
+    e.preventDefault();
+    dispatch(tes(!loading));
+  };
 
-	return (
-		<>
-			<div className="w-screen h-screen bg-[#251E34]">
-				<SIdeBar />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="w-screen h-screen bg-[#251E34] flex">
+        <SIdeBar />
+        <div className="flex-1 w-full pt-36 justify-center items-center">
+          <div className="flex justify-center items-center relative h-full">
+            <div className="p-10 absolute rotate-[-20deg] left-[10%] hover:z-20 hover:scale-105 transition-all delay-75 ease-in-out hover:drop-shadow-md">
+              <CardHomePage urlCard={"../../public/assets/card/orange-card.png"} />
+            </div>
+            <div className="p-10 absolute rotate-[-10deg] left-[25%] top-[10%] hover:z-20 hover:scale-105 transition-all delay-75 ease-in-out">
+              <CardHomePage urlCard={"../../public/assets/card/blue-card.png"}/>
+            </div>
+            <div className="p-10 absolute rotate-[10deg] left-[40%] top-[11%] hover:z-20 hover:scale-105 transition-all delay-75 ease-in-out">
+              <CardHomePage urlCard={"../../public/assets/card/magenta-card.png"}/>
+            </div>
+            <div className="p-10 absolute rotate-[20deg] left-[55%] hover:z-20 hover:scale-105 transition-all delay-75 ease-in-out">
+              <CardHomePage urlCard={"../../public/assets/card/purple-card.png"}/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
