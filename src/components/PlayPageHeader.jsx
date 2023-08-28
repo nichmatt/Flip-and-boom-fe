17d85dd128eea39d111b4dd6e1b01de685425bdd
dhannywijaya <dhannywijaya1997@gmail.com>
2023-08-28 09:53:00 +0700

    feat : some update

--- src/components/PlayPageHeader.jsx
+++ src/components/PlayPageHeader.jsx
@@ -1,7 +1,6 @@
 import { useSelector } from "react-redux";
 
-import { useEffect } from "react";
-
+import defaultChar from "/assets/character/default.png";
 import mrKing from "/assets/character/mr-king.png";
 
 import NumberTween from "./NumberTween";
@@ -9,14 +8,10 @@ import NumberTween from "./NumberTween";
 export default function PlayPageHeader({ hp, enemyHp }) {
 	const { selectedChar } = useSelector((state) => state.selectedReducer);
 
-	useEffect(() => {
-		console.log(hp, enemyHp);
-	}, []);
-
 	return (
 		<div className="flex h-32 mx-44 justify-between text-white italic font-bold">
 			<div className="w-32 h-32 rotate-[10deg]">
-				<img src={selectedChar == "mrKing" ? mrKing : null} />
+				<img src={selectedChar == "mrKing" ? mrKing : defaultChar} />
 			</div>
 			<div className="flex flex-col justify-end pb-7">
 				<div className="text-2xl flex justify-between px-4 mb-1">
@@ -27,7 +22,9 @@ export default function PlayPageHeader({ hp, enemyHp }) {
 				</div>
 				<div className="h-6 w-[25rem] bg-black rounded-2xl border">
 					<div
-						className={`h-full bg-[#2EFF0C] rounded-2xl duration-1000`}
+						className={`h-full bg-[${
+							hp > 500 ? "#2EFF0C" : hp > 250 ? "#FFAC0C" : "#ff0c0c"
+						}] rounded-2xl duration-1000`}
 						style={{ width: `${hp / 10}%` }}
 					></div>
 				</div>
@@ -42,7 +39,9 @@ export default function PlayPageHeader({ hp, enemyHp }) {
 				</div>
 				<div className="h-6 w-[25rem] bg-black rounded-2xl border">
 					<div
-						className={`h-full bg-[#FFAC0C] rounded-2xl duration-1000`}
+						className={`h-full bg-[${
+							enemyHp > 500 ? "#2EFF0C" : enemyHp > 250 ? "#FFAC0C" : "#ff0c0c"
+						}] rounded-2xl duration-1000`}
 						style={{ width: `${enemyHp / 10}%` }}
 					></div>
 				</div>
@@ -51,7 +50,7 @@ export default function PlayPageHeader({ hp, enemyHp }) {
 				className="w-32 h-32"
 				style={{ transform: "scaleX(-1) rotate(10deg)" }}
 			>
-				<img src={selectedChar == "mrKing" ? mrKing : null} />
+				<img src={selectedChar == "mrKing" ? mrKing : mrKing} />
 			</div>
 		</div>
 	);
