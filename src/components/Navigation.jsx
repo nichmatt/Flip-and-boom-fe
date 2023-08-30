822f45d9c784c2e87ac42eddfbbb402f74d41513
nichmatt <nicholas.official17@gmail.com>
2023-08-30 20:02:53 +0700

    unregisteredPage

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,18 +1,20 @@
 import { useSelector } from "react-redux";
 
+// import "./YourComponent.css";
+
 import { experience } from "../helpers";
 
 export default function NavigationBar() {
-	const { profile } = useSelector((state) => state.userReducer);
+  const { profile } = useSelector((state) => state.userReducer);
 
-	return (
-		<>
-			<section
-				id="NavigationBar-Section"
-				className="h-[80px] bg-[#2a2550] w-[100vw] mw-[100vw] font-basefont font-semibold tracking-tight z-10"
-				style={{ position: "fixed ", top: "0" }}
-			>
-				<div className="h-[25px] bg-[#ffb800]">
+  return (
+    <>
+      <section
+        id="NavigationBar-Section"
+        className="h-[80px] bg-[#2a2550] w-[100vw] mw-[100vw] font-basefont font-semibold tracking-tight z-10"
+        style={{ position: "fixed ", top: "0" }}
+      >
+        {/* <div className="h-[25px] bg-[#ffb800]">
 					<marquee
 						direction="left"
 						behavior="scroll|slide|alternate"
@@ -23,25 +25,30 @@ export default function NavigationBar() {
 					>
 						Welcome to Flip & Boom
 					</marquee>
-				</div>
-				<div className="flex justify-end py-4 mx-[10vw]">
-					<i
-						className="fa-solid fa-money-bill-1-wave text-[#ffb800] py-[4px] "
-						style={{ textShadow: "0.5px 0.1px 5px" }}
-					></i>
-					<p className="pl-[10px] text-[#ffb800] italic">:</p>
-					<p
-						style={{ textShadow: "1px 1px 10px" }}
-						className="pr-[30px] pl-[10px] text-[#ffb800] italic"
-					>
-						{profile?.balance}
-					</p>
-					<p className="px-[30px] text-[#fff] italic">
-						LV : {experience(profile?.experience)}
-					</p>
-					<p className="px-[30px] text-[#fff] italic">{profile?.username}</p>
-				</div>
-			</section>
-		</>
-	);
+				</div> */}
+        <div className="h-[25px] bg-[#ffb800] flex justify-center">
+          <div className="continuous-slide scrolling-text animate__animated animate__slideInLeft w-[80%]">
+            Welcome to Flip &amp; Boom
+          </div>
+        </div>
+        <div className="flex justify-end py-4 mx-[10vw]">
+          <i
+            className="fa-solid fa-money-bill-1-wave text-[#ffb800] py-[4px] "
+            style={{ textShadow: "0.5px 0.1px 5px" }}
+          ></i>
+          <p className="pl-[10px] text-[#ffb800] italic">:</p>
+          <p
+            style={{ textShadow: "1px 1px 10px" }}
+            className="pr-[30px] pl-[10px] text-[#ffb800] italic"
+          >
+            {profile?.balance}
+          </p>
+          <p className="px-[30px] text-[#fff] italic">
+            LV : {experience(profile?.experience)}
+          </p>
+          <p className="px-[30px] text-[#fff] italic">{profile?.username}</p>
+        </div>
+      </section>
+    </>
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import 'animate.css';
+import "animate.css";
+import "animate.css/animate.min.css";
 
 import { Provider } from "react-redux";
 import store from "./stores/index.js";
@@ -11,9 +12,9 @@ import router from "./routers/router";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<RouterProvider router={router} />
-		</Provider>
-	</React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
 );
